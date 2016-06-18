de44f9e8861048837a4e60e296c49db7456f1c5c
Norbert <balazsnorbi@gmail.com>
2016-06-18 14:00:24 +0300

    Initial commit.

---
+++ app/src/main/java/com/example/norbert/foodcourttm/ScrollingActivity.java
@@ -0,0 +1,75 @@
+package com.example.norbert.foodcourttm;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private TextView webContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        webContent = (TextView) findViewById(R.id.web_content);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Refreshing data!", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                downloadWebContent();
+            }
+        });
+        downloadWebContent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void downloadWebContent() {
+        if (isNetworkConnected()) {
+            new DownloadWebPageTask(webContent).execute(URLContainer.AZI_LA_PRANZ);
+        } else {
+//            TODO: No internet: how to handle it?
+        }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
---
+++ app/src/main/java/com/example/norbert/foodcourttm/URLContainer.java
@@ -0,0 +1,9 @@
+package com.example.norbert.foodcourttm;
+
+/**
+ * Created by Norbert on 6/18/2016.
+ */
+public class URLContainer {
+
+    public static final String AZI_LA_PRANZ = "http://m.azilapranz.ro/timisoara/";
+}
