3bcc7eb2892ca70a2bcfe404faca73fd26ac2f48
Norbert <balazsnorbi@gmail.com>
2016-06-18 14:43:17 +0300

    Initial commit.

--- app/src/main/java/com/example/norbert/foodcourttm/DownloadWebPageTask.java
+++ app/src/main/java/com/example/norbert/foodcourttm/DownloadWebPageTask.java
@@ -4,12 +4,19 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 /**
  * Created by Norbert on 6/18/2016.
@@ -20,7 +27,7 @@ import java.net.URL;
  * an InputStream. Finally, the InputStream is converted into a string, which is
  * displayed in the UI by the AsyncTask's onPostExecute method.
  */
-public class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+public class DownloadWebPageTask extends AsyncTask<String, Void, Document> {
 
     private TextView textView;
 
@@ -29,61 +36,21 @@ public class DownloadWebPageTask extends AsyncTask<String, Void, String> {
     }
 
     @Override
-    protected String doInBackground(String... urls) {
-
-        // params comes from the execute() call: params[0] is the url.
+    protected Document doInBackground(String... urls) {
+        Document document = null;
         try {
-            return downloadUrl(urls[0]);
+            document = Jsoup.connect(urls[0]).get();
         } catch (IOException e) {
-            return "Unable to retrieve web page. URL may be invalid.";
+            e.printStackTrace();
         }
+
+        return document;
     }
 
     // onPostExecute displays the results of the AsyncTask.
     @Override
-    protected void onPostExecute(String result) {
-        textView.setText(result);
-    }
-
-    // Given a URL, establishes an HttpUrlConnection and retrieves
-// the web page content as a InputStream, which it returns as
-// a string.
-    private String downloadUrl(String myurl) throws IOException {
-        InputStream is = null;
-
-        try {
-            URL url = new URL(myurl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            // Starts the query
-            conn.connect();
-            int response = conn.getResponseCode();
-            Log.d("DownloadWebPageTask", "The response is: " + response);
-            is = conn.getInputStream();
-
-            // Convert the InputStream into a string
-            return readIt(is);
-
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-        } finally {
-            if (is != null) {
-                is.close();
-            }
-        }
-    }
-
-    // Reads an InputStream and converts it to a String.
-    private String readIt(InputStream stream) throws IOException {
-        Reader reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[100];
-        StringBuilder stringBuilder = new StringBuilder();
-        while (reader.read(buffer) > 0) {
-            stringBuilder.append(buffer);
-        }
-        return stringBuilder.toString();
+    protected void onPostExecute(Document document) {
+        Element listItems = document.select("ul").get(0);
+        Elements restaurants = listItems.select("li");
     }
 }
